4e8294c46930a3b3b3fbb48df5d1ef2804f4aa39
Mark Gerrior <github@gerrior.com>
2020-07-10 09:38:13 -0700

    ex4 successful solution.

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -3,24 +3,31 @@ def has_negatives(a):
     input = {} # dictionary 
 
     # convert to dictionary
-    # Value is True if negative number found. 
+    # I was just going to test if True but the problem is I don't know the order the + and - will come in. 
     for number in a:
-        if number in input:
+        if abs(number) in input:
+            # already in dictionary
             if number < 0:
-                input[abs(number)] = True
-        else:
-            if number < 0:
-                input[abs(number)] = True
+                if input[abs(number)] == "+":
+                    input[abs(number)] = "+-"
             else: 
-                input[number] = False
+                if input[number] == "-":
+                    input[number] = "+-"
+        else:
+            # Add to dictionary
+            if number < 0:
+                input[abs(number)] = "-"
+            else: 
+                input[number] = "+"
 
 
-    for theValue, foundNegative in input.items():
-        if foundNegative == True:
+    for theValue, wereBothFound in input.items():
+        if wereBothFound == "+-":
             result.append(theValue)
 
     return result
 
 
 if __name__ == "__main__":
+#    print(has_negatives([1,2,3,-4]))
     print(has_negatives([-1, -2, 1, 2, 3, 4, -4]))
