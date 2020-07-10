d16e2ae2ef84e1309cf2d5bbe3e1b92c2e993fdf
Mark Gerrior <github@gerrior.com>
2020-07-10 09:00:00 -0700

    Completed ex2. All tests pass.

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -9,6 +9,24 @@ def reconstruct_trip(tickets, length):
     """
     YOUR CODE HERE
     """
-    # Your code here
+    route = [] # arrray
+    itinerary = {} # dictionary 
+
+    # convert to dictionary
+    for ticket in tickets:
+        itinerary[ticket.source] = ticket.destination
+
+    # Find the first airport
+    airport = itinerary["NONE"]
+
+    # do while in python
+    # Build route until we get to NONE
+    while True: 
+        route.append(airport)
+
+        if airport == "NONE":
+            break
+
+        airport = itinerary[airport]
 
     return route
